adb7bcf7ea6e5f619991318d20a2196db7657ea8
Jose Motta <josemotta@bampli.com>
2021-09-28 18:23:07 -0300

    usando regra validacao category/genre

--- app/Http/Controllers/Api/VideoController.php
+++ app/Http/Controllers/Api/VideoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Models\Video;
+use App\Rules\GenresHasCategoriesRule;
 use Illuminate\Http\Request;
 
 class VideoController extends BasicCrudController
@@ -19,12 +20,17 @@ class VideoController extends BasicCrudController
             'rating' => 'required|in:' . implode(',', Video::RATING_LIST),
             'duration' => 'required|integer',
             'categories_id' => 'required|array|exists:categories,id,deleted_at,NULL',
-            'genres_id' => 'required|array|exists:genres,id,deleted_at,NULL'
+            'genres_id' => [
+                'required', // style for merging at addRuleIfGenreHasCategories()
+                'array',
+                'exists:genres,id,deleted_at,NULL'
+            ]
         ];
     }
 
     public function store(Request $request)
     {
+        $this->addRuleIfGenreHasCategories($request);
         $validatedData = $this->validate($request, $this->rulesStore());
         $self = $this;
 
@@ -42,6 +48,7 @@ class VideoController extends BasicCrudController
     public function update(Request $request, $id)
     {
         $obj = $this->findOrFail($id);
+        $this->addRuleIfGenreHasCategories($request);
         $validatedData = $this->validate($request, $this->rulesUpdate());
         $self = $this;
 
@@ -51,6 +58,15 @@ class VideoController extends BasicCrudController
             return $obj;
         });
         return $obj;
+    }
+
+    protected function addRuleIfGenreHasCategories($request)
+    {
+        $categoriesId = $request->get('categories_id');
+        $categoriesId = is_array($categoriesId) ? $categoriesId : [];
+        $this->rules['genres_id'][] = new GenresHasCategoriesRule(
+            $categoriesId
+        );
     }
 
     protected function handleRelations($video, Request $request)
