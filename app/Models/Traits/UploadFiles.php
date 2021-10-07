2790c28e28c688cc30451ebd17ee8b857e1fe67a
Jose Motta <josemotta@bampli.com>
2021-10-07 15:36:21 -0300

    criando e testando metodos para excluir arquivos

--- app/Models/Traits/UploadFiles.php
+++ app/Models/Traits/UploadFiles.php
@@ -2,6 +2,8 @@
 
 namespace App\Models\Traits;
 
+use Exception;
+use Illuminate\Contracts\Container\BindingResolutionException;
 use Illuminate\Http\UploadedFile;
 
 trait UploadFiles
@@ -23,5 +25,23 @@ trait UploadFiles
     public function uploadFile(UploadedFile $file)
     {
         $file->store($this->uploadDir());
+    }
+
+    public function deleteFiles(array $files)
+    {
+        foreach ($files as $file) {
+            $this->deleteFile($file);
+        }
+    }
+
+    /**
+     * 
+     * @param string|UploadedFile $file 
+     * @return void 
+     */
+    public function deleteFile($file)
+    {
+        $filename = $file instanceof UploadedFile ? $file->hashName() : $file;
+        \Storage::delete("{$this->uploadDir()}/{$filename}");
     }
 }
