86a49e96a03e31a32dd1eedfbafb8f9a89326bb1
Jose Motta <josemotta@bampli.com>
2021-10-13 13:19:03 -0300

    separando testes crud/upload video

---
+++ tests/Feature/Models/Video/VideoUploadTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Feature\Models\Video;
+
+use App\Models\Category;
+use App\Models\Video;
+use App\Models\Genre;
+use Illuminate\Database\QueryException;
+
+class VideoUploadTest extends BaseVideoTestCase
+{
+    
+}
