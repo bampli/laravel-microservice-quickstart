5828622c301fbd2b31ba69034ec579c63e5758cf
Jose Motta <josemotta@bampli.com>
2021-11-02 12:05:42 -0300

    add assertIfFilesUrlExist

--- tests/Feature/Http/Controllers/Api/VideoController/BaseVideoControllerTestCase.php
+++ tests/Feature/Http/Controllers/Api/VideoController/BaseVideoControllerTestCase.php
@@ -57,7 +57,15 @@ abstract class BaseVideoControllerTestCase extends TestCase
     {
         parent::setUp();
         $this->video = factory(Video::class)->create([
-            'opened' => false
+            'opened' => false,
+            'video_file' => "video_file.mp4",
+            'thumb_file' => "thumb_file.jpg",
+            'banner_file' => "banner_file.jpg",
+            'trailer_file' => "trailer_file.mp4",
+            // 'video_file' => UploadedFile::fake()->create("video_file.mp4"),
+            // 'thumb_file' => UploadedFile::fake()->create("thumb_file.jpg"),
+            // 'banner_file' => UploadedFile::fake()->create("banner_file.jpg"),
+            // 'trailer_file' => UploadedFile::fake()->create("trailer_file.mp4"),
         ]);
         $category = factory(Category::class)->create();
         $genre = factory(Genre::class)->create();
@@ -69,7 +77,7 @@ abstract class BaseVideoControllerTestCase extends TestCase
             'rating' => Video::RATING_LIST[0],
             'duration' => 90,
             'categories_id' => [$category->id],
-            'genres_id' => [$genre->id]
+            'genres_id' => [$genre->id],
         ];
     }
 
@@ -80,7 +88,7 @@ abstract class BaseVideoControllerTestCase extends TestCase
         $data = array_key_exists(0, $data) ? $data[0] : $data;
         foreach ($fileFields as $field) {
             $file = $video->{$field};
-            dump($data, $field, $data[$field . '_url']);
+            //dump($data, $field, $data[$field . '_url']);
             $this->assertEquals(
                 \Storage::url($video->relativeFilePath($file)),
                 $data[$field . '_url']
